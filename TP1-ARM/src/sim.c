46a7ccfb1c6bae9b8f311e9844774d4b3901efea
joacoszterensus <jszterensus@udesa.edu.ar>
2025-03-28 12:24:35 -0300

    por ahora funciona

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -33,19 +33,19 @@ typedef struct {
 } InstructionEntry;
 
 InstructionEntry instruction_set[] = {
-    {0b10110001     << 24, 0xFF000000, execute_adds_inmediate},        //B1
-    {0b101010110000 << 20, 0xFFF00000, execute_adds_extended_register},//AB0
-    {0b11010001     << 24, 0xFF000000, execute_sub_inmediate},         //D1
-    {0b111010110000 << 20, 0xFFF00000, execute_sub_extended_register}, //EB0
-    {0b110101000100 << 20, 0xFFF00000, execute_HLT},                   //D44
-    {0b111010110010 << 20, 0xFFF00000, execute_CMP_extended_register}, //EB1
-    {0b11110001     << 24, 0xFF000000, execute_CMP_inmediate},         //F1
-    {0b11101010     << 24, 0xFF000000, execute_ands_shifted_register}, //EA
-    {0b11001010     << 24, 0xFF000000, execute_eor_shifted_register},  //CA
-    {0b10101010     << 24, 0xFF000000, execute_orr_shifted_register},  //AA
-    {0b000101       << 26, 0xFC000000, execute_b_target},              //03
-    {0b1101011000011111000000<< 10, 0xFFFFFC00, execute_br},   
-    {0b01010100     << 24, 0xFF000000, execute_b_conditional},         //54  
+    {0xB1     << 24, 0xFF000000, execute_adds_inmediate},        
+    {0xAB0    << 20, 0xFFF00000, execute_adds_extended_register},
+    {0xF1     << 24, 0xFF000000, execute_sub_inmediate},         
+    {0xEB0    << 20, 0xFFF00000, execute_sub_extended_register}, 
+    {0xD44    << 20, 0xFFF00000, execute_HLT},                   
+    {0xEB0    << 20, 0xFFF00000, execute_CMP_extended_register}, 
+    {0xF0     << 24, 0xFF000000, execute_CMP_inmediate},         
+    {0xEA     << 24, 0xFF000000, execute_ands_shifted_register}, 
+    {0xCA     << 24, 0xFF000000, execute_eor_shifted_register},  
+    {0xAA     << 24, 0xFF000000, execute_orr_shifted_register},  
+    {0x03     << 26, 0xFC000000, execute_b_target},              
+    {0x3587C0 << 10, 0xFFFFFC00, execute_br},   
+    {0x54     << 24, 0xFF000000, execute_b_conditional},         
     
                   
     {0, 0, NULL} 
@@ -80,15 +80,15 @@ int64_t signextend64(uint32_t value, int bits) {
 }
 
 void execute_adds_inmediate(uint32_t instruction) {
-    uint32_t rd = (instruction >> 0) & 0x1F;   // Bits 0-4: Rd
-    uint32_t rn = (instruction >> 5) & 0x1F;   // Bits 5-9: Rn
-    uint32_t imm12 = (instruction >> 10) & 0xFFF; // Bits 10-21: imm12
-    uint32_t shift = (instruction >> 22) & 0x3; // Bits 22-23: shift
+    uint32_t rd = (instruction >> 0) & 0x1F;   
+    uint32_t rn = (instruction >> 5) & 0x1F;   
+    uint32_t imm12 = (instruction >> 10) & 0xFFF; 
+    uint32_t shift = (instruction >> 22) & 0x3; 
 
     uint64_t operand2 = imm12;
 
     if (shift == 0b01) {
-        operand2 <<= 12;  // LSL #12
+        operand2 <<= 12;  
     } 
 
     uint64_t result = CURRENT_STATE.REGS[rn] + operand2;
@@ -102,15 +102,15 @@ void execute_adds_inmediate(uint32_t instruction) {
 }
 
 void execute_adds_extended_register(uint32_t instruction) {
-    uint32_t rd = (instruction >> 0) & 0x1F;   // Bits 0-4: Rd
-    uint32_t rn = (instruction >> 5) & 0x1F;   // Bits 5-9: Rn
-    uint32_t rm = (instruction >> 16) & 0x1F;  // Bits 16-20: Rm
-    uint32_t shift = (instruction >> 22) & 0x3; // Bits 22-23: shift
+    uint32_t rd = (instruction >> 0) & 0x1F;   
+    uint32_t rn = (instruction >> 5) & 0x1F;   
+    uint32_t rm = (instruction >> 16) & 0x1F;  
+    uint32_t shift = (instruction >> 22) & 0x3; 
 
     uint64_t operand2 = CURRENT_STATE.REGS[rm];
 
     if (shift == 0b01) {
-        operand2 <<= 12;  // LSL #12
+        operand2 <<= 12;  
     } 
 
     uint64_t result = CURRENT_STATE.REGS[rn] + operand2;
@@ -124,47 +124,48 @@ void execute_adds_extended_register(uint32_t instruction) {
 }
 
 void execute_sub_inmediate(uint32_t instruction) {
-    uint32_t rd = (instruction >> 0) & 0x1F;   // Bits 0-4: Rd
-    uint32_t rn = (instruction >> 5) & 0x1F;   // Bits 5-9: Rn
-    uint32_t imm12 = (instruction >> 10) & 0xFFF; // Bits 10-21: imm12
-    uint32_t shift = (instruction >> 22) & 0x3; // Bits 22-23: shift
+    uint32_t rd    = (instruction >> 0) & 0x1F;     
+    uint32_t rn    = (instruction >> 5) & 0x1F;      
+    uint32_t imm12 = (instruction >> 10) & 0xFFF;     
+    uint32_t shift = (instruction >> 22) & 0x3;       
 
     uint64_t operand2 = imm12;
 
     if (shift == 0b01) {
-        operand2 <<= 12;  // LSL #12
+        operand2 <<= 12;  
     } 
 
-    int64_t result = CURRENT_STATE.REGS[rn] - operand2;
-    NEXT_STATE.REGS[rd] = -result;
-
-    NEXT_STATE.FLAG_N = (result >> 63) & 1;        
-    NEXT_STATE.FLAG_Z = (result == 0);            
-
-    NEXT_STATE.PC = CURRENT_STATE.PC + 4;
-
-}
-
-void execute_sub_extended_register(uint32_t instruction) {
-    uint32_t rd = (instruction >> 0) & 0x1F;   // Bits 0-4: Rd
-    uint32_t rn = (instruction >> 5) & 0x1F;   // Bits 5-9: Rn
-    uint32_t rm = (instruction >> 16) & 0x1F;  // Bits 16-20: Rm
-    uint32_t shift = (instruction >> 22) & 0x3; // Bits 22-23: shift
-
-    uint64_t operand2 = CURRENT_STATE.REGS[rm];
-
-    if (shift == 0b01) {
-        operand2 <<= 12;  // LSL #12
-    } 
-
-    int64_t result = CURRENT_STATE.REGS[rn] - operand2;
+    operand2 = ~operand2;  
+    int64_t result = CURRENT_STATE.REGS[rn] + operand2 + 1;  
+    
     NEXT_STATE.REGS[rd] = result;
 
     NEXT_STATE.FLAG_N = (result >> 63) & 1;        
     NEXT_STATE.FLAG_Z = (result == 0);            
 
     NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+}
 
+void execute_sub_extended_register(uint32_t instruction) {
+    uint32_t rd    = (instruction >> 0) & 0x1F;      
+    uint32_t rn    = (instruction >> 5) & 0x1F;      
+    uint32_t rm    = (instruction >> 16) & 0x1F;     
+    uint32_t shift = (instruction >> 22) & 0x3;      
+
+    uint64_t operand2 = CURRENT_STATE.REGS[rm];
+
+    if (shift == 0b01) {
+        operand2 <<= 12;  
+    } 
+
+    operand2 = ~operand2;  
+    int64_t result = CURRENT_STATE.REGS[rn] + operand2 + 1;  
+    NEXT_STATE.REGS[rd] = result;
+
+    NEXT_STATE.FLAG_N = (result >> 63) & 1;        
+    NEXT_STATE.FLAG_Z = (result == 0);            
+
+    NEXT_STATE.PC = CURRENT_STATE.PC + 4;
 }
 
 void execute_HLT(uint32_t instruction) {
@@ -177,19 +178,18 @@ void execute_HLT(uint32_t instruction) {
 }
 
 void execute_CMP_inmediate(uint32_t instruction) {
-    uint32_t rn = (instruction >> 5) & 0x1F;     // Bits 5-9: Rn (registro fuente)
-    uint32_t imm12 = (instruction >> 10) & 0xFFF; // Bits 10-21: imm12
-    uint32_t shift = (instruction >> 22) & 0x3;  // Bits 22-23: shift
+    uint32_t rn = (instruction >> 5) & 0x1F;     
+    uint32_t imm12 = (instruction >> 10) & 0xFFF; 
+    uint32_t shift = (instruction >> 22) & 0x3;  
 
     uint64_t operand2 = imm12;
 
     if (shift == 0b01) {
-        operand2 <<= 12;  // LSL #12
+        operand2 <<= 12;  
     } 
 
     uint64_t result = CURRENT_STATE.REGS[rn] - operand2;
 
-    // Actualizar flags de condición
     NEXT_STATE.FLAG_N = (result >> 63) & 1; 
     NEXT_STATE.FLAG_Z = (result == 0);      
     
@@ -198,14 +198,14 @@ void execute_CMP_inmediate(uint32_t instruction) {
 }
 
 void execute_CMP_extended_register(uint32_t instruction) {
-    uint32_t rn = (instruction >> 5) & 0x1F;   // Bits 5-9: Rn
-    uint32_t rm = (instruction >> 16) & 0x1F;  // Bits 16-20: Rm
-    uint32_t shift = (instruction >> 22) & 0x3; // Bits 22-23: shift
+    uint32_t rn = (instruction >> 5) & 0x1F;   
+    uint32_t rm = (instruction >> 16) & 0x1F; 
+    uint32_t shift = (instruction >> 22) & 0x3; 
 
     uint64_t operand2 = CURRENT_STATE.REGS[rm];
 
     if (shift == 0b01) {
-        operand2 <<= 12;  // LSL #12
+        operand2 <<= 12;  
     } 
 
     uint64_t result = CURRENT_STATE.REGS[rn] - operand2;
@@ -218,9 +218,9 @@ void execute_CMP_extended_register(uint32_t instruction) {
 }
 
 void execute_ands_shifted_register(uint32_t instruction) {
-    uint32_t rd = (instruction >> 0) & 0x1F;   // Bits 0-4: Rd
-    uint32_t rn = (instruction >> 5) & 0x1F;   // Bits 5-9: Rn
-    uint32_t rm = (instruction >> 16) & 0x1F;  // Bits 16-20: Rm
+    uint32_t rd = (instruction >> 0) & 0x1F;   
+    uint32_t rn = (instruction >> 5) & 0x1F;  
+    uint32_t rm = (instruction >> 16) & 0x1F;  
 
     uint64_t operand2 = CURRENT_STATE.REGS[rm];
 
@@ -275,7 +275,7 @@ void execute_br(uint32_t instruction) {
 }
 
 void execute_b_conditional(uint32_t instruction) {
-    uint32_t cond = (instruction) & 0xF; //condition code
+    uint32_t cond = (instruction) & 0xF; 
     
     for (int i = 0; b_cond_set[i].execute != NULL; i++) {
         if ((cond & b_cond_set[i].mask) == b_cond_set[i].opcode) {
@@ -292,6 +292,9 @@ void execute_b_eq(uint32_t instruction) {
         
         NEXT_STATE.PC = CURRENT_STATE.PC + offset;     
     }
+    else{
+        NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+    }
 }
 
 void execute_b_ne(uint32_t instruction) {
@@ -300,6 +303,9 @@ void execute_b_ne(uint32_t instruction) {
         int64_t offset = signextend64(imm19, 19) << 2;
         
         NEXT_STATE.PC = CURRENT_STATE.PC + offset;    
+    }
+    else{
+        NEXT_STATE.PC = CURRENT_STATE.PC + 4;
     }
 }
 
@@ -310,6 +316,9 @@ void execute_b_gt(uint32_t instruction) {
         
         NEXT_STATE.PC = CURRENT_STATE.PC + offset;    
     }
+    else{
+        NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+    }
 }
 
 void execute_b_lt(uint32_t instruction) {
@@ -318,6 +327,9 @@ void execute_b_lt(uint32_t instruction) {
         int64_t offset = signextend64(imm19, 19) << 2;
         
         NEXT_STATE.PC = CURRENT_STATE.PC + offset;    
+    }
+    else{
+        NEXT_STATE.PC = CURRENT_STATE.PC + 4;
     }
 }
 
@@ -328,6 +340,9 @@ void execute_b_ge(uint32_t instruction) {
         
         NEXT_STATE.PC = CURRENT_STATE.PC + offset;    
     }
+    else{
+        NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+    }
 }
 
 void execute_b_le(uint32_t instruction) {
@@ -336,6 +351,9 @@ void execute_b_le(uint32_t instruction) {
         int64_t offset = signextend64(imm19, 19) << 2;
         
         NEXT_STATE.PC = CURRENT_STATE.PC + offset;    
+    }
+    else{
+        NEXT_STATE.PC = CURRENT_STATE.PC + 4;
     }
 }
 
